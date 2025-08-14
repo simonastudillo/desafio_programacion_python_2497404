b7afa882631ed38f44f1ae0096da2d0a579f1d4b
Simon Astudillo <simon.astudillo@raef.cl>
2025-08-14 15:57:05 -0300

    identificar anagrama

--- 01_desafios_texto/01_04_anagramas.py
+++ 01_desafios_texto/01_04_anagramas.py
@@ -1,4 +1,4 @@
-def es_anagrama(palabra_1, palabra_2):
+def anagrama(palabra_1, palabra_2):
 
     letras_palabra_1 = sorted(palabra_1.lower())
     letras_palabra_2 = sorted(palabra_2.lower())
@@ -6,6 +6,7 @@ def es_anagrama(palabra_1, palabra_2):
     return letras_palabra_1 == letras_palabra_2
 
 
-print(es_anagrama("lama", "Mala"))   # True
-print(es_anagrama("calor", "coral")) # True
-print(es_anagrama("cama", "casa"))   # False
+print(anagrama("lama", "Mala"))   # True
+print(anagrama("calor", "coral")) # True
+print(anagrama("gato", "gota")) # True
+print(anagrama("cama", "casa"))   # False
