5846463448d76e6f01c4ef2a2f92f4343b348d64
Simon Astudillo <simon.astudillo@raef.cl>
2025-08-14 11:00:49 -0300

    Corrección de estandar

--- 01_desafios_texto/01_02_primera_letra_repetida.py
+++ 01_desafios_texto/01_02_primera_letra_repetida.py
@@ -1,14 +1,14 @@
-def firstLetterRepeted(text):
-  readedCharacters = []
+def first_letter_repeted(text):
+  readed_characters = []
   text = text.lower()
   text = text.replace(" ", "")
   for character in text:
-    if character in readedCharacters:
+    if character in readed_characters:
       return character
     else:
-      readedCharacters.append(character)
+      readed_characters.append(character)
   return None
 
-print(firstLetterRepeted("Hello World!"))  # {'l': 3, 'o': 2}
-print(firstLetterRepeted("Hola mundo!"))  # {'o': 2}
-print(firstLetterRepeted("hola"))  # None
+print(first_letter_repeted("Hello World!"))  # {'l': 3, 'o': 2}
+print(first_letter_repeted("Hola mundo!"))  # {'o': 2}
+print(first_letter_repeted("hola"))  # None
