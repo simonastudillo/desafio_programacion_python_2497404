61cd808881770e4c035e360a0c1b12a98f332608
Simon Astudillo <simon.astudillo@raef.cl>
2025-08-14 21:07:53 -0300

    calcular el factorial

--- 02_desafios_numericos/02_02_factorial.py
+++ 02_desafios_numericos/02_02_factorial.py
@@ -1,16 +1,19 @@
-def calcular_factorial(numero):
+def factorial(numero):
+  if numero < 0:
+    return "Error: No se puede calcular el factorial de un número negativo."
+  elif numero == 0 or numero == 1:
+    return 1
+  else:
+    resultado = 1
+    for i in range(2, numero + 1):
+      resultado *= i
+    return resultado
 
-    factorial = 1
-    for i in range(1, numero+1):
-        factorial = factorial * i
 
-    return factorial
-
-
-print(calcular_factorial(0)) # 1
-print(calcular_factorial(3)) # 6
-print(calcular_factorial(4)) # 24
-print(calcular_factorial(5)) # 120
+print(factorial(0)) # 1
+print(factorial(3)) # 6
+print(factorial(4)) # 24
+print(factorial(5)) # 120
 
 
 def calcular_factorial_recursivo(numero):
