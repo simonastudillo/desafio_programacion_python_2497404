e700dfaef9bc7a611a23f04e2584c08b4b0a8fe9
Simon Astudillo <spastudilloq@gmail.com>
2025-08-14 23:12:37 -0300

    Convertir una lista anidada en una lista unidimensional

--- 03_desafios_estructuras_datos/03_03_aplanar_lista.py
+++ 03_desafios_estructuras_datos/03_03_aplanar_lista.py
@@ -1,3 +1,21 @@
+def flattening(array):
+   if type(array) is not list:
+      return array
+   new_list = []
+   repeat = False
+   for element in array:
+      if type(element) is list:
+         new_list.extend(element)
+         repeat = True
+      else:
+         new_list.append(element)
+   if repeat:
+      return flattening(new_list)
+   return new_list
+
+print(flattening([2, 3, 4, [3, 2]])) # [2, 3, 4, 3, 2]
+print(flattening([2, 3, 4, [[2],[6,5]]])) # [2, 3, 4. [2]]
+
 def aplanar_lista(lista):
 
     nueva_lista = []
